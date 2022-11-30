8ddd1573f328681dc10361126d38532d56a9c15b
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2022-11-29 20:50:56 -0500

    worked on ingredients adding

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 document.querySelector("button").addEventListener("click", getDrink)
 
+
 function getDrink () {
-  let drink =  document.querySelector("input").value;
+  /* Set drink variable to users input */
+  let drink =  "Lemon Drop"
+  // document.querySelector("input").value;
 
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
                     /*Gets query value from drink input*/
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
-      console.log(data.drinks);
-      document.querySelector("img").src = data.drinks[0].strDrinkThumb;
-      document.querySelector(".name").innerText = data.drinks[0].strDrink;
-      document.querySelector(".instructions").innerText = data.drinks[0].strInstructions;
-      // document.querySelector(".ingredients").innerText = data.
+      const prefix = data.drinks[0];
+      console.log(data.drinks)
+      document.querySelector("img").src = prefix.strDrinkThumb
+      document.querySelector(".name").innerText = prefix.strDrink
+      document.querySelector(".instructions").innerText = prefix.strInstructions
+
+      //! Add ingredients
+      
     })
     .catch((err) => {
       console.log(`error ${err}`);
