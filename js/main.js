3fabffa507defa8c2f6b219360df11fd2b954ad8
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2023-01-21 16:00:59 -0500

    ingredients list added

--- js/main.js
+++ js/main.js
@@ -16,8 +16,36 @@ function getDrink () {
       document.querySelector("img").src = prefix.strDrinkThumb
       document.querySelector(".name").innerText = prefix.strDrink
       document.querySelector(".instructions").innerText = prefix.strInstructions
+      document.querySelector('ul').innerText = ''
 
       //! Add ingredients
+      function createListOfIngredients(measure, ingredient) {
+        const li = document.createElement('li')
+        if (measure && ingredient) {
+          li.innerText = `${measure} ${ingredient}`
+        } else {
+          li.innerText = `${ingredient}`
+        }
+        document.querySelector('ul').appendChild(li)
+      }
+
+      const ingredientSubstring = 'strIngredient'
+      const measureSubstring = 'strMeasure'
+      const drink = data.drinks[0]
+      let measure = ''
+      let ingredient = ''
+
+      for (const [key, value] of Object.entries(drink)) {
+
+        if (key.includes(ingredientSubstring) && value) {
+          let measureKey = measureSubstring + key[key.length - 1]
+          ingredient = value
+          measure = drink[measureKey]
+          console.log(`${measure} ${ingredient}`)
+          createListOfIngredients(measure, ingredient)
+        }
+      }  
+
       
     })
     .catch((err) => {
