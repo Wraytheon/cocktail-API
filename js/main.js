5ff0cf0eaf100b220aeb8155f77b69cca741ddd0
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2023-02-04 19:20:21 -0500

    random and search functions display name, ingredients, instructions horizontally, no images displayed

--- js/main.js
+++ js/main.js
@@ -1,55 +1,83 @@
-//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
-// document.querySelector("button").addEventListener("click", getDrink)
+// Constants to store the API URL for searching and getting a random drink
+const apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+const randomApiUrl = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+
+// Selecting the search button, random button, input field, and result container element from the HTML
+const searchBtn = document.querySelector("#search-btn");
+const randomBtn = document.querySelector("#random-btn");
+const input = document.querySelector("#input");
 
 
-window.onload = function getDrink () {
-  /* Set drink variable to users input */
-  let drink =  "Lemon Drop"
-  // document.querySelector("input").value;
+// Adding a click event listener to the search button
+searchBtn.addEventListener("click", function () {
+  // Get the value of the input field
+  const drinkName = input.value;
 
-  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
-                    /*Gets query value from drink input*/
-    .then((res) => res.json()) // parse response as JSON
+  // If the input field is empty, return and do nothing
+  if (!drinkName) return;
+
+  // Fetch the data from the API URL by concatenating the input value to the apiUrl constant
+  fetch(apiUrl + drinkName)
+    .then((res) => res.json()) // Convert the response to JSON
     .then((data) => {
-      const prefix = data.drinks[0];
-      console.log(data.drinks)
-      document.querySelector("img").src = prefix.strDrinkThumb
-      document.querySelector(".name").innerText = prefix.strDrink
-      document.querySelector(".instructions").innerText = prefix.strInstructions
-      document.querySelector('ul').innerText = ''
+      // Get the first drink from the data and store it in a variable
+      const drink = data.drinks[0];
 
-      //! Add ingredients
-      function createListOfIngredients(measure, ingredient) {
-        const li = document.createElement('li')
-        if (measure && ingredient) {
-          li.innerText = `${measure} ${ingredient}`
-        } else {
-          li.innerText = `${ingredient}`
-        }
-        document.querySelector('ul').appendChild(li)
+      // Get the drink name, ingredients, and instructions
+      const drinkName = drink.strDrink;
+      const ingredientArr = [];
+      for (let i = 1; i <= 15; i++) {
+        // Check if there is no ingredient at the current iteration, break the loop
+        if (!drink[`strIngredient${i}`]) break;
+        // Push the ingredient and its measure to the ingredientArr array
+        ingredientArr.push(
+          `${drink[`strIngredient${i}`]} - ${drink[`strMeasure${i}`]}`
+        );
       }
+      const ingredients = ingredientArr.join(", ")
+      const instructions = drink.strInstructions;
 
-      const ingredientSubstring = 'strIngredient'
-      const measureSubstring = 'strMeasure'
-      const drink = data.drinks[0]
-      let measure = ''
-      let ingredient = ''
+      // Update the HTML of the result container with the drink details
+      document.getElementById("result-container").classList.remove("hide")
 
-      for (const [key, value] of Object.entries(drink)) {
+      document.getElementById("drinkName").innerHTML = drinkName;
 
-        if (key.includes(ingredientSubstring) && value) {
-          let measureKey = measureSubstring + key[key.length - 1]
-          ingredient = value
-          measure = drink[measureKey]
-          console.log(`${measure} ${ingredient}`)
-          createListOfIngredients(measure, ingredient)
-        }
-      }  
+      document.querySelector(".instructions").textContent = instructions;
 
-      
-    })
-    .catch((err) => {
-      console.log(`error ${err}`);
+      document.querySelector(".ingredients").textContent = ingredients
     });
-}
+});
 
+// Adding a click event listener to the random button
+randomBtn.addEventListener("click", function () {
+  // Fetch the data from the random API URL
+  fetch(randomApiUrl)
+    .then((res) => res.json()) // Convert the response to JSON
+    .then((data) => {
+      // Get the first drink from the data and store it in a variable
+      const drink = data.drinks[0];
+
+      // Get the drink name, ingredients, and instructions
+      const drinkName = drink.strDrink;
+      const ingredientArr = [];
+      for (let i = 1; i <= 15; i++) {
+        // Check if there is no ingredient at the current iteration, break the loop
+        if (!drink[`strIngredient${i}`]) break;
+        // Push the ingredient and its measure to the ingredientArr array
+        ingredientArr.push(
+          `${drink[`strIngredient${i}`]} - ${drink[`strMeasure${i}`]}`
+        );
+      }
+      const ingredients = ingredientArr.join(", ")
+      const instructions = drink.strInstructions;
+
+      // Update the HTML of the result container with the drink details
+      document.getElementById("result-container").classList.remove("hide")
+
+      document.getElementById("drinkName").innerHTML = drinkName;
+
+      document.querySelector(".instructions").textContent = instructions;
+
+      document.querySelector(".ingredients").textContent = ingredients
+    });
+});
